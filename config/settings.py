1b6023827e282b36a391b223117a9ba999cae1c5
koeunseo <rhdmstj17@cau.ac.kr>
2021-02-10 02:14:45 +0900

    static_root 변수 설정(배포중..)

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
