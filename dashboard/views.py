ff464fe8aa6445f4fa07b91f9ed979e997b4a193
koeunseo <rhdmstj17@cau.ac.kr>
2021-02-20 00:46:16 +0900

    [UPDATE] requirements.txt 불필요한 패키기 모두 삭제

--- dashboard/views.py
+++ dashboard/views.py
@@ -18,19 +18,37 @@ class GithubUserView(APIView) :
         username = request.GET['username'] # username 받을거임
         url = 'https://api.github.com/users/%s' % username
         urlresponse = requests.get(url)
-        # print(urlresponse.json())
+        print("그냥 response", urlresponse)
+        print("json()형식으로!!", urlresponse.json()) # json() -> response를 json타입으로 인코딩되어 반환
         # JSON 잘 넘김
         ctx = urlresponse.json()
+        # print(type(ctx)) # 딕셔너리타입임
         data = {
             'username' : ctx['login'],
             'profile_img_url' : ctx['avatar_url']
         }
-        json_data = json.dumps(data)
-        # print(data, type(json_data))
-        
-        return JsonResponse(data, safe=False) #data(dict)가 맞는 지 json_data(str) 맞는 지 헷갈림
+        json_data = json.dumps(data) #dictionary를 json으로 변환
+        # print(type(json_data)) # json.dumps를 쓰면 str타입(이게json타입이군..)으로 바뀌군..
+        # json을 dictionary로 변환하고 싶다면 json.loads(json_val)
+        return Response(data)  #data(dict)가 맞는 지 json_data(str) 맞는 지 헷갈림 
+        # response는 렌더링되지 않은 컨텐츠를 가져와 올바른 타입으로 변환해준다.
 
-    
+
+# class GithubLanguageView(APIView) :
+#     def get(self, request):
+#         username = request.GET['username'] # username 받을거임
+#         url = 'https://api.github.com/users/%s' % username
+#         urlresponse = requests.get(url)
+#         print("그냥 response", urlresponse)
+        
+#         ctx = urlresponse.json() # json() -> response를 json타입으로 인코딩되어 반환
+#         # print(type(ctx)) # 딕셔너리타입임
+#         data = {
+#             'username' : ctx['login'],
+#             'profile_img_url' : ctx['avatar_url']
+#         }
+#         return Response(data)  
+
 # @api_view(['GET'])
 # def events(request):
 #     body = request.data
